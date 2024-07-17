ce286c1711e1458ba0290c031e61f49d1ab2450d
ElementalDLC37 <lucasantony815@gmail.com>
2024-07-17 01:38:54 -0300

    edit default app to create a new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FormComponent } from './component/form/form.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ApresentationComponent } from "./component/apresentation/apresentation.component";
+import { CardListComponent } from './component/card-list/card-list.component';
+import { LegendComponent } from './component/legend/legend.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FormComponent, ApresentationComponent, CardListComponent, LegendComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

b9d7e1969edb2c785124faf156dd7f5fb38190cd
ElementalDLC37 <lucasantony815@gmail.com>
2024-07-18 15:40:56 -0300

    Fix erros in locations service

--- src/app/service/locations.service.ts
+++ src/app/service/locations.service.ts
@@ -1,160 +1,181 @@
-import { Schedule } from './../types/Schedule.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Locations } from '../types/Locations.interface';
 import { Location } from '../types/Location.interface';
 
+// Serviço responsável por administrar os filtros das localizações
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocationsService {
-  private apiUrl: string = "https://test-frontend-developer.s3.amazonaws.com/data/locations.json";
+  private apiUrl: string = 'https://test-frontend-developer.s3.amazonaws.com/data/locations.json'
 
-  private current_country_id: number = 0;
-  private locationsComplete: Location[] = [];
-  private locationsFiltered: Location[] = [];
+  // Listas para a filtragem das localizações
+  private allLocations: Location[] = []
+  private filteredLocations: Location[] = []
 
-  private rangeHoursOfDayFilter: number[] = [];
-  private filterByEndWeekDays: string[] = [];
-
+  // Observer utilizado para fazer o stream de dados entre os componentes que necessitam
   private locationsSubject: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([])
-  locations: Observable<Location[]> = this.locationsSubject.asObservable();
+  locations: Observable<Location[]> = this.locationsSubject.asObservable()
 
+  // Construtor realizando o request, armazenamento
   constructor(private httpClient: HttpClient) {
-    this.httpClient.get<Locations>(this.apiUrl).subscribe(data => {
-      this.current_country_id = data.current_country_id;
-      this.locationsComplete = data.locations;
+    this.httpClient.get<Locations>(this.apiUrl).subscribe((data) => {
+      this.allLocations = data.locations
 
-      this.locationsSubject.next(data.locations)
+      this.locationsSubject.next(this.allLocations)
     })
   }
 
   getLocations(hour: string, openSaturday: boolean, openMonday: boolean, showClosed: boolean): void {
-    switch(hour) {
-      case 'morning':
-        this.rangeHoursOfDayFilter[0] = 6
-        this.rangeHoursOfDayFilter[1] = 12
-        break
-      case 'afternoon':
-        this.rangeHoursOfDayFilter[0] = 12
-        this.rangeHoursOfDayFilter[1] = 18
-        break
-      case 'evening':
-        this.rangeHoursOfDayFilter[0] = 18
-        this.rangeHoursOfDayFilter[1] = 23
-        break
-      default:
-        this.rangeHoursOfDayFilter.pop()
-    }
+    this.filteredLocations = this.isShowClosed(showClosed)
+    this.filteredLocations = this.isOpenSaturday(openSaturday, hour)
+    this.filteredLocations = this.isOpenMonday(openMonday, hour)
+    this.filteredLocations = this.isOpen(hour)
 
-    this.filterByEndWeekDays.pop()
-    this.filterByEndWeekDays.pop()
+    this.locationsSubject.next(this.filteredLocations)
+  }
 
+  private isShowClosed(showClosed: boolean): Location[] {
+    return showClosed ? this.allLocations : this.allLocations.filter(location => location.opened == true)
+  }
+
+  private isOpenSaturday(openSaturday: boolean, hour: string): Location[] {
     if(openSaturday) {
-      this.filterByEndWeekDays.push("Sáb.")
-    }
+      return this.filteredLocations.filter(location => {
+        if(location.schedules) {
+          return location.schedules.filter(week => {
+            if ((week.weekdays == 'Sáb.') && (week.hour !== 'Fechada')) {
+              // Caso não seja requisitado filtro por hora, já termina o filtro por aqui
+              if(!Boolean(hour)) return true
 
-    if (openMonday) {
-      this.filterByEndWeekDays.push("Dom.")
-    }
+              // Dados de abertura e fechamento de determinada localização
+              let openAt = Number(week.hour.slice(0, 2))
+              let closeAt = Number(week.hour.slice(-3, -1))
 
-    this.locationsFiltered = this.isShowClosed(showClosed, this.locationsComplete);
-    this.locationsFiltered = this.isOpenEndWeek(showClosed, openSaturday, openMonday, this.locationsFiltered);
-    this.locationsFiltered = this.isHour(showClosed, openSaturday, openMonday, this.locationsFiltered);
+              // Inicializa as váriaveis para armazenarem o filtro requisitado no formulário
+              let openRequired = 0
+              let closeRequired = 0
 
-    this.locationsSubject.next(this.locationsFiltered)
-  }
-
-  private isShowClosed(showClosed: boolean, locations: Location[]): Location[] {
-    if(!showClosed) {
-      return locations.filter(location => location.opened == true)
-    } else {
-      return locations
-    }
-  }
-
-  private isOpenEndWeek(showClosed: boolean, openSaturday: boolean, openMonday: boolean, locations: Location[]): Location[] {
-    return locations.filter(location => {
-      if(location.schedules) {
-        let indexWeekday = ''
-        return location.schedules.filter(week => {
-          if(week.weekdays === 'Sáb.' || week.weekdays === 'Dom.') {
-            if(!(week.hour === 'Fechada')) {
-              if(this.filterByEndWeekDays[0] === week.weekdays) {
-                if(!(indexWeekday === week.weekdays)) {
-                  indexWeekday = week.weekdays
-                  return true
-                }
+              switch (hour) {
+                case 'morning':
+                  openRequired = 6
+                  closeRequired = 12
+                  break
+                case 'afternoon':
+                  openRequired = 12
+                  closeRequired = 18
+                  break
+                case 'evening':
+                  openRequired = 18
+                  closeRequired = 23
+                  break
               }
+
+              // Verifica se a academia fecha antes da abertura requerida e se abre antes do fechamento requerido
+              return closeAt > openRequired && openAt < closeRequired
             }
-          }
-          return false
-        }).length >= (Number(openSaturday) + Number(openMonday))
-      } else {
-        if(showClosed) {
-          return true
+
+            return false
+          }).length > 0
         }
+
+        return false
+      })
+    }
+
+    return this.filteredLocations
+  }
+
+  private isOpenMonday(openMonday: boolean, hour: string): Location[] {
+    if(openMonday) {
+      return this.filteredLocations.filter(location => {
+        if(location.schedules) {
+
+          return location.schedules.filter(week => {
+            if ((week.weekdays == 'Dom.') && (week.hour !== 'Fechada')) {
+              // Caso não seja requisitado filtro por hora, já termina o filtro por aqui
+              if(!Boolean(hour)) return true
+
+              // Dados de abertura e fechamento de determinada localização
+              let openAt = Number(week.hour.slice(0, 2))
+              let closeAt = Number(week.hour.slice(-3, -1))
+
+              // Inicializa as váriaveis para armazenarem o filtro requisitado no formulário
+              let openRequired = 0
+              let closeRequired = 0
+
+              switch (hour) {
+                case 'morning':
+                  openRequired = 6
+                  closeRequired = 12
+                  break
+                case 'afternoon':
+                  openRequired = 12
+                  closeRequired = 18
+                  break
+                case 'evening':
+                  openRequired = 18
+                  closeRequired = 23
+                  break
+              }
+
+              // Verifica se a academia fecha antes da abertura requerida e se abre antes do fechamento requerido
+              return closeAt > openRequired && openAt < closeRequired
+            }
+
+            return false
+          }).length > 0
+        }
+
+        return false
+      })
+    }
+
+    return this.filteredLocations
+  }
+
+  private isOpen(hour: string): Location[] {
+    if(!Boolean(hour)) return this.filteredLocations
+
+    return this.filteredLocations.filter(location => {
+      if(location.schedules) {
+        return location.schedules.filter(week => {
+          if ((week.weekdays == 'Seg. à Sex.') && (week.hour !== 'Fechada')) {
+            // Dados de abertura e fechamento de determinada localização
+            let openAt = Number(week.hour.slice(0, 2))
+            let closeAt = Number(week.hour.slice(-3, -1))
+
+            // Inicializa as váriaveis para armazenarem o filtro requisitado no formulário
+            let openRequired = 0
+            let closeRequired = 0
+
+            switch (hour) {
+              case 'morning':
+                openRequired = 6
+                closeRequired = 12
+                break
+              case 'afternoon':
+                openRequired = 12
+                closeRequired = 18
+                break
+              case 'evening':
+                openRequired = 18
+                closeRequired = 23
+                break
+            }
+
+            // Verifica se a academia fecha antes da abertura requerida e se abre antes do fechamento requerido
+            return closeAt > openRequired && openAt < closeRequired
+          }
+
+          return false
+        }).length > 0
       }
+
       return false
     })
   }
 
-  private isHour(showClosed: boolean, openSaturday: boolean, openMonday: boolean, locations: Location[]): Location[] {
-    if((this.rangeHoursOfDayFilter.length > 1)) {
-      if(openSaturday || openMonday) {
-        locations.filter(location => {
-          if(location.schedules) {
-            let ifDouble = false
-            let ifSingle = false
-
-            return location.schedules.filter(week => {
-              if(!(week.hour == "Fechada") && ((week.weekdays === "Sáb.") || (week.weekdays === "Dom."))) {
-                let openAt = Number(week.hour.slice(0, 2))
-                let closeAt = Number(week.hour.slice(-3, -1))
-
-                if((week.weekdays === this.filterByEndWeekDays[1])) {
-                  ifDouble = (!(closeAt <= this.rangeHoursOfDayFilter[0]) && !(this.rangeHoursOfDayFilter[1] <= openAt))
-                }
-
-                if((week.weekdays === this.filterByEndWeekDays[0])) {
-                  ifSingle = (!(closeAt <= this.rangeHoursOfDayFilter[0]) && !(this.rangeHoursOfDayFilter[1] <= openAt))
-                }
-
-                switch(this.filterByEndWeekDays.length) {
-                  case 1:
-                    return ifSingle
-                  case 2:
-                    return ifSingle || ifDouble
-                }
-              }
-
-              return false
-            }).length > 0
-          }
-
-          return false
-        })
-      } else {
-        return locations.filter(location => {
-          if(location.schedules) {
-            return location.schedules.filter(week => {
-              if(week.hour == "Fechada") return false
-
-              let openAt = Number(week.hour.slice(0, 2))
-
-              return !(this.rangeHoursOfDayFilter[1] <= openAt)
-
-            }).length > 0
-          } else {
-            if(showClosed) {
-              return true
-            }
-          }
-          return false
-        })
-      }
-    }
-    return locations;
-  }
 }
